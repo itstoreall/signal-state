93f8d784a2baf7b97500dfb5f8145ac4bcfff0c6
Serhii Stanislav <serhiistanislav@gmail.com>
2024-01-08 22:54:35 +0200

    ADD | an initial state

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat', // Must be below test-utils
+      'react/jsx-runtime': 'preact/jsx-runtime'
+    }
+  },
+  server: {
+    port: 3000
+  }
+});
+
+/*
+// import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
   }
 });
+*/
