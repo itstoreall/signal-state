56158cb8082266ce3890011051e0705cf59dfbd5
Serhii Stanislav <serhiistanislav@gmail.com>
2024-01-14 23:02:20 +0200

    ADD | localStorage logic

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,10 +2,32 @@ import One from '../One';
 import Two from '../Two';
 import Three from '../Three';
 import s from './Layout.module.scss';
+import { useEffect } from 'react';
 
 const Layout = () => {
   console.log('start');
   console.log('');
+
+  const stateLSLabel = 'signal_state';
+
+  useEffect(() => {
+    const retrievedText = localStorage.getItem(stateLSLabel);
+
+    if (retrievedText) {
+      console.log('stateLSLabel ----->', retrievedText);
+      localStorage.removeItem(stateLSLabel);
+    }
+
+    const setDataBeforeUnload = () => {
+      localStorage.setItem(stateLSLabel, 'localStorage data');
+    };
+
+    window.addEventListener('beforeunload', setDataBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', setDataBeforeUnload);
+    };
+  }, []);
 
   return (
     <div className={s.wrapper}>
