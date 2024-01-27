7e09f7cdc29742060c4c6cc135e0c06646ccd965
Serhii Stanislav <serhiistanislav@gmail.com>
2024-01-27 19:20:53 +0200

    EDIT | code refactoring

--- src/signalState/types/index.ts
+++ src/signalState/types/index.ts
@@ -1,8 +1,8 @@
 import { Signal } from '@preact/signals';
 
 export type Val = number | string;
-
 export type State = { val: Val; isStore: boolean };
+export type Config = { key: string; state: State };
 
 // ------ Basic:
 
@@ -10,22 +10,12 @@ export type Get = () => Val;
 export type Set = (val: Val, dly: number) => void;
 export type IsStore = boolean;
 
+// ------ Context:
+
+export type Context = { [key: string]: StateFunctions };
+export type StateFunctions = { get: Get; set: Set; isStore: IsStore };
+export type BasicRecord = Record<string, StateFunctions>;
+
 // ------ utils:
 
 export type SetState = (sig: Signal<State>, val: Val, dly: number) => void;
-
-// ------ Config:
-
-export type Config = { key: string; state: State };
-
-// ------ State:
-
-export type StateFunctions = { get: Get; set: Set; isStore: IsStore };
-
-export type BasicRecord = Record<string, StateFunctions>;
-
-// ------ Context:
-
-export type Context = {
-  [key: string]: StateFunctions;
-};
